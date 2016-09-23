07ff8751d6b08bc5035d51e79c00cdacceb4478f
anshul.abhinav@centurylink.com <anshul.abhinav@centurylink.com>
2016-09-23 11:42:59 -0600

    Updates to the solution

--- Day_0/Solution.java
+++ Day_0/Solution.java
@@ -14,6 +14,9 @@ public class Solution {
 		// Print a string literal saying "Hello, World." to stdout.
 		System.out.println("Hello, World.");
       
-	    // TODO: Write a line of code here that prints the contents of inputString to stdout.
+	    // Write a line of code here that prints the contents of inputString to stdout.
+	    System.out.println("Hello, World." + inputString);
+
+
 	}
 }
